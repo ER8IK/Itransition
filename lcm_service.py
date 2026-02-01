5dd0a91aefa2957cfcc7e0682fc982578d17146f
ER8IK <locha.eth@gmail.com>
2026-02-01 17:13:04 +0400

    final commit

--- lcm_service.py
+++ lcm_service.py
@@ -3,15 +3,20 @@ from flask import Flask, request
 app = Flask(__name__)
 
 def gcd(a, b):
+    """Вычисление НОД через алгоритм Евклида"""
     while b:
         a, b = b, a % b
     return a
 
 def lcm(x, y):
+    """Вычисление НОК"""
     return abs(x * y) // gcd(x, y)
 
-@app.route('/dedppoolbhfg@gmail.com', methods=['GET'])
+@app.route('/dedppoolbhfg_gmail_com', methods=['GET'])  # ← ИСПРАВЛЕНО: @ заменено на _
 def calculate_lcm():
+    """
+    Веб-метод для вычисления НОК двух натуральных чисел.
+    """
     try:
         x_param = request.args.get('x')
         y_param = request.args.get('y')
@@ -33,4 +38,7 @@ def calculate_lcm():
         return 'NaN'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    import os
+    # ← ИСПРАВЛЕНО: Динамический порт для Render
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
